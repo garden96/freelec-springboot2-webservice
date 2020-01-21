dfee9976550daf55029b491780d64ab5e25bd482
jeongown.an <garden96@gmail.com>
2020-01-21 19:06:31 +0900

    5.3 JPA Auditing 으로 생성시간/수정시간 자동화하기

--- src/main/java/net/sunnygarden/book/springboot/Application.java
+++ src/main/java/net/sunnygarden/book/springboot/Application.java
@@ -2,7 +2,9 @@ package net.sunnygarden.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing      // Enables JPA Auditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/net/sunnygarden/book/springboot/domain/posts/Posts.java
+++ src/main/java/net/sunnygarden/book/springboot/domain/posts/Posts.java
@@ -3,13 +3,14 @@ package net.sunnygarden.book.springboot.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import net.sunnygarden.book.springboot.domain.BaseTimeEntity;
 
 import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
