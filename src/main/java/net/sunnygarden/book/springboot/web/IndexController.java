384cb4c49fc2b4431a251980ed2c876acb62f131
jeongown.an <garden96@gmail.com>
2020-01-28 20:38:09 +0900

    ~ p.157  등록글 수정 기능 추가.

--- src/main/java/net/sunnygarden/book/springboot/web/IndexController.java
+++ src/main/java/net/sunnygarden/book/springboot/web/IndexController.java
@@ -2,9 +2,12 @@ package net.sunnygarden.book.springboot.web;
 
 import lombok.RequiredArgsConstructor;
 import net.sunnygarden.book.springboot.service.PostsService;
+import net.sunnygarden.book.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -23,4 +26,11 @@ public class IndexController {
         return "posts-save";
     }
 
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
 }
